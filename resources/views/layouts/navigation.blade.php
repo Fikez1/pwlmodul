333cace317c71ea73b0b48f64b2de8c9f7a3c0d6
Fikez1 <Fikriggz69@gmail.com>
2023-12-08 19:33:49 +0700

    Modul 5

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,8 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-            </div>
 
+            </div>
+           
+            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('role')" :active="request()->routeIs('role')">
+                        {{ __('Role') }}
+                    </x-nav-link>
+                </div>
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
